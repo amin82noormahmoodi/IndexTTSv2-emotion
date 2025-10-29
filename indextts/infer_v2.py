4772498effac1f3b0d59409ace934448b8b7a1ba
amin82noormahmoodi <aminnoormahmoodi2@gmail.com>
2025-10-29 12:33:28 +0330

    code updated

--- indextts/infer_v2.py
+++ indextts/infer_v2.py
@@ -701,11 +701,12 @@ def find_most_similar_cosine(query_vector, matrix):
 class QwenEmotion:
     def __init__(self, model_dir):
         self.model_dir = model_dir
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_dir)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_dir, trust_remote_code=True)
         self.model = AutoModelForCausalLM.from_pretrained(
             self.model_dir,
             torch_dtype="float16",  # "auto"
-            device_map="auto"
+            device_map="auto",
+            trust_remote_code=True
         )
         self.prompt = "文本情感分类"
         self.cn_key_to_en = {
